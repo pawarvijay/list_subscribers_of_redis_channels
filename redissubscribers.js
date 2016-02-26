793fb9261b49c71f53d1fcbeb58b35d649b1ad68
vijay <vijay.pawar@bizos.net>
2016-02-26 13:01:49 +0530

    first commit

---
+++ redissubscribers.js
@@ -0,0 +1,46 @@
+//@jonatas Walker
+//1 : Befor running this code i did
+//127.0.0.1:6379> CLIENT SETNAME 'YOVIJAY'
+
+//2 : My two channels
+//127.0.0.1:6379> PUBSUB CHANNELS 
+//1) "events"
+//2) "commands"
+
+var redis = require('redis');
+var redis_client = redis.createClient();       
+var found;
+
+redis_client.multi().client(['list']).exec(function(err, results) {
+
+ 	found = false;
+
+	//console.log('results : ' + results);
+
+  	var pairs = results[0].split(' ');
+
+	//console.log('pairs : ' + pairs);
+
+	  pairs.forEach(function(pair){
+
+	    var kv = pair.split('=');
+
+		//console.log('single pairs = : ' + pairs);
+
+		//console.log('single kv = : ' + kv);
+                 
+		//console.log('single kv[0] = : ' + kv[0]);
+
+		//console.log('single kv[1] = : ' + kv[1]);
+
+	    	if (kv[0] == 'YOOVIJAY' && kv[1] == 'YOOVIJAY')
+		{
+	      		found = true;
+			console.log('single kv = : ' + kv);
+			found = false;
+		}	
+	  });
+});
+
+
+
